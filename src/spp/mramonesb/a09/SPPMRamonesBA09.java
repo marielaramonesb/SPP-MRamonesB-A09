5af8763cc01db79560d0353da79477a20a7feb1f
marielaramonesbalvoa <marielaramonesbalvoa@192.168.1.68>
2017-03-07 22:20:14 -0600

    matrices.

--- src/spp/mramonesb/a09/SPPMRamonesBA09.java
+++ src/spp/mramonesb/a09/SPPMRamonesBA09.java
@@ -15,73 +15,75 @@ public class SPPMRamonesBA09 {
      * @param args the command line arguments
      */
   public static void main(String[] args) {
-int matriz [][]=new int [99][99];
-int matrizb [][]=new int [99][99];
-Scanner keyboard=new Scanner (System.in);
-
+int size = insertartamaño();
+        int [][] A = arreglo ("A", size);
+        int [][] B = arreglo ("B", size);
+        
+        sumamatriz (A,B);
+       
+    }
+    public static int insertartamaño(){
+        Scanner keyboard = new Scanner (System.in);
+        boolean flag;
+        int A = 0;
+        do{
+            try{
+                System.out.println("Ingresa tamaño del arreglo: ");
+                A = keyboard.nextInt();
+                flag = false;
+            }
+            catch (Exception e){
+                System.out.println("ERROR");
+                keyboard.next();
+                flag = true;
+            }
+        }
+        while (flag);
+        return A;
+    }
+    public static int solicitarentero(){
+        Scanner keyboard = new Scanner (System.in);
+        boolean flag;
+        int A = 0;
+        do{
+            try{
+                System.out.println("Ingresa un entero: ");
+                A= keyboard.nextInt();
+                flag = false;
+            }
+            catch (Exception e){
+                System.out.println("Error");
+                keyboard.next();
+                flag = true;
+            }
+        }
+        while (flag);
+        return A;
+    }
+    public static int [][] arreglo (String A, int tamaño){
+        int [][]arreglo = new int [tamaño][tamaño];
+        System.out.println("Arreglo: " + A);
+        for (int i = 0; i < arreglo.length; i++){
+        for (int j = 0; j < arreglo[i].length; j++){
+        arreglo [i][j] = solicitarentero();
+        }
+        }
+        return arreglo;
+    }
+    //realizar la suma de matrices en un metodo
     
-//matriz1
-System.out.println("ingresa la primera matriz");
-int cont=keyboard.nextInt (); 
-System.out.println("ingresa las filas de la primera matriz");
-int cont2=keyboard.nextInt (); 
-//matriz
-for(int x=0;x<=cont;x++)
-{
-for(int y=0;y<=cont2;y++)
-{
-System.out.println("ingresa el valor en "+x+","+y);
-int valor=keyboard.nextInt (); 
-matriz [x][y]=valor;
+    public static int [][] sumamatriz (int [][]A, int[][]B){
+        int [][] R = new int [A.length] [A.length];
+        System.out.println("Suma: ");
+        for (int i = 0; i < A.length; i++){
+        for (int j = 0; j < A[i].length; j++){
+        R[i][j] = A[i][j] + B[i][j];
+        System.out.println("["+R[i][j]+"]");   
+    }
+        System.out.println("");   
+    }
+        return R; 
+    }
+    
+    
 }
-  }
-
-//matriz2
-System.out.println("ingresa las columnas de la segunda matriz");
-int cont3=keyboard.nextInt (); 
-System.out.println("ingresa las filasde la segunda matriz");
-int cont4=keyboard.nextInt (); 
-//matriz 2
-for(int x=0;x<=cont3;x++)
-{
-for(int y=0;y<=cont4;y++)
-{
-System.out.println("ingresa el valor en "+x+","+y);
-int valor=keyboard.nextInt (); 
-matrizb [x][y]=valor;
-}
-  }
-//matriz1
-for (int x=0;x<=cont;x++)//imprimiendo con el for
-{
-for (int y=0;y<=cont2;y++)//imprimiendo con el for
-{
-System.out.print(matriz[x][y]);
-} 
-System.out.println("\t");
-}
-System.out.println("es la matriz A");
-//matriz 2 
-for (int x=0;x<=cont3;x++)//imprimiendo con el for
-{
-for (int y=0;y<=cont4;y++)//imprimiendo con el for
-{
-System.out.print(matrizb[x][y]);
-} 
-System.out.println("\t");
-}
-System.out.println("es la matriz B");
-//realizando suma de las 
-for (int x=0;x<=cont3;x++)//imprimiendo con el for
-{
-for (int y=0;y<=cont4;y++)//imprimiendo con el for
-{
-int res=matriz[x][y]+matrizb[x][y];
-System.out.print("\t"+res);
-}
-System.out.println("");
-}
-System.out.println("\t... es la matriz resultado");
-}
-}
-
